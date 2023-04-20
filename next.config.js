44b63dcd567d273295fdffa9ed7066a79f272247
liushuxiang <276900785@qq.com>
2023-04-21 00:31:43 +0800

    fixed

--- next.config.js
+++ next.config.js
@@ -9,7 +9,6 @@ const { i18n } = require("./next-i18next.config");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
-  // reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
