8514f668b1c1585d7efbba2bc4ed88c81567b859
Ethan Liu <liushuxiang19930729@gmail.com>
2023-06-25 18:57:17 +0800

    test cron console

--- src/app/api/cron/cost/route.ts
+++ src/app/api/cron/cost/route.ts
@@ -1,54 +1,55 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+// import { prisma } from "@/lib/prisma";
 import { LResponseError } from "@/lib";
 
 export async function GET() {
   try {
     console.log("\n\n");
     console.log(process.env.CRON_SECRET, "CRON_SECRET");
+    console.log("12345");
     console.log("\n\n");
 
-    const costs = await prisma.cost.findMany({
-      where: {
-        createdAt: {
-          gte: new Date(new Date().setHours(0, 0, 0, 0)),
-        },
-      },
-    });
+    // const costs = await prisma.cost.findMany({
+    //   where: {
+    //     createdAt: {
+    //       gte: new Date(new Date().setHours(0, 0, 0, 0)),
+    //     },
+    //   },
+    // });
 
-    // if costs exist means already run today
-    if (costs.length) return LResponseError("already run today");
+    // // if costs exist means already run today
+    // if (costs.length) return LResponseError("already run today");
 
-    const users = await prisma.user.findMany({
-      where: {
-        costTokens: {
-          gt: 0,
-        },
-      },
-    });
+    // const users = await prisma.user.findMany({
+    //   where: {
+    //     costTokens: {
+    //       gt: 0,
+    //     },
+    //   },
+    // });
 
-    if (!users.length) return LResponseError("no users need to cost");
+    // if (!users.length) return LResponseError("no users need to cost");
 
-    for (const user of users) {
-      const { id, availableTokens, costTokens, costUSD } = user;
+    // for (const user of users) {
+    //   const { id, availableTokens, costTokens, costUSD } = user;
 
-      await prisma.cost.create({
-        data: {
-          costTokens,
-          costUSD,
-          userId: id,
-        },
-      });
+    //   await prisma.cost.create({
+    //     data: {
+    //       costTokens,
+    //       costUSD,
+    //       userId: id,
+    //     },
+    //   });
 
-      await prisma.user.update({
-        data: {
-          costTokens: 0,
-          costUSD: 0,
-          availableTokens: availableTokens - costTokens,
-        },
-        where: { id: user.id },
-      });
-    }
+    //   await prisma.user.update({
+    //     data: {
+    //       costTokens: 0,
+    //       costUSD: 0,
+    //       availableTokens: availableTokens - costTokens,
+    //     },
+    //     where: { id: user.id },
+    //   });
+    // }
 
     return NextResponse.json({ error: 0 }, { status: 200 });
   } catch (error) {
