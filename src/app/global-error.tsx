b5ba0eeca6d0ff8f56119a37086069010f14ffbf
Ethan Liu <liushuxiang19930729@gmail.com>
2023-07-12 22:12:22 +0800

    update

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -10,7 +10,7 @@ export default function GlobalError({
   return (
     <html>
       <body>
-        <h2>Something went wrong!</h2>
+        <h2>Something went wrong</h2>
         <button onClick={() => reset()}>Try again</button>
       </body>
     </html>
