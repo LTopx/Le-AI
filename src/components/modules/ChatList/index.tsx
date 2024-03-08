a60c697da22ee4e0e9676e190239c392026a873d
Ethan <ethan@zolplay.com>
2024-03-08 14:38:10 +0800

    feat: add loading state in chatList

--- src/components/modules/ChatList/index.tsx
+++ src/components/modules/ChatList/index.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef } from 'react'
 
+import { cn } from '@/lib/utils'
 import { LOADING_STATE, Message, useChatStore } from '@/store/chat'
 
 import { ChatItem } from './Item'
@@ -24,7 +25,11 @@ export function ChatList() {
   return (
     <div className="flex-1 overflow-y-auto" ref={scrollRef}>
       <div className="container max-w-4xl px-5">
-        <div className="flex flex-col gap-7 py-10">
+        <div
+          className={cn('flex flex-col gap-7 pb-10 pt-10', {
+            'pb-0': activeChat.chat_state !== LOADING_STATE.NONE,
+          })}
+        >
           {activeChat.chat_list.map((item, index) => (
             <ChatItem
               key={item.id}
@@ -33,6 +38,18 @@ export function ChatList() {
             />
           ))}
         </div>
+        {activeChat.chat_state !== LOADING_STATE.NONE && (
+          <div className="mt-4 flex items-center gap-2 pb-10 pl-12 text-muted-foreground">
+            <span className="i-mingcute-loading-line h-[18px] w-[18px] animate-spin" />
+            <span>
+              AI is{' '}
+              {activeChat.chat_state === LOADING_STATE.CONNECTING
+                ? 'thinking'
+                : 'typing'}
+              ...
+            </span>
+          </div>
+        )}
       </div>
     </div>
   )
