bb204eb28f7bde16485420e9db30d880c56fbb96
Ethan <ethan@zolplay.com>
2024-03-13 21:18:13 +0800

    feat: add github link

--- src/components/modules/SideMenus/footer.tsx
+++ src/components/modules/SideMenus/footer.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { useCommonStore } from '@/store/common'
 
 export function Footer() {
@@ -5,9 +7,13 @@ export function Footer() {
 
   return (
     <div className="flex h-12 items-center justify-between border-t border-t-zinc-300 px-2.5">
-      <div className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-md transition-colors hover:bg-[#cccdce]">
-        <span className="i-mingcute-settings-4-line" />
-      </div>
+      <Link
+        className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-md transition-colors hover:bg-[#cccdce]"
+        href="https://github.com/LTopx/Le-AI"
+        target="_blank"
+      >
+        <span className="i-ri-github-fill" />
+      </Link>
       <div
         className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-md transition-colors hover:bg-[#cccdce]"
         onClick={() => updateOpen()}
